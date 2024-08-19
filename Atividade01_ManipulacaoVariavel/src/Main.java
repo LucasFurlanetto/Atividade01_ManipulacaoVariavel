fe07792cf9cae13f9b34394236113a1d8cbfda88
LucasFurlanetto <127804969+LucasFurlanetto@users.noreply.github.com>
2024-08-19 16:16:56 -0300

    Add files via upload

---
+++ Atividade01_ManipulacaoVariavel/src/Main.java
@@ -0,0 +1,20 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+
+    String nome = "cleiton";
+    int idade = 23;
+    float altura = 1.90f;
+    boolean estudante = true;
+
+    int idadePessoa1 = 45;
+    int idadePessoa2 = 60;
+
+    Integer soma = idadePessoa1 + idadePessoa2;
+    Integer multiplicacao = (int) (altura * 2);
+
+    String frase = "Olá[nome]";
+
+
+}
